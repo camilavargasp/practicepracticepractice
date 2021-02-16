ad1fb837b3ed5c13e4ccb84218e38aa4695d1447
camilavargasp <cvargas@bren.ucsb.edu>
2021-02-16 11:25:23 -0800

    mix things

---
+++ Carpentry/R_ecology_lesson/load_data.R
@@ -0,0 +1,8 @@
+##Load data
+
+download.file(url = "https://ndownloader.figshare.com/files/2292169",
+              destfile = "/Users/camilavargaspoulsen/github/practicepracticepractice/Carpentry/R_ecology_lesson/portal_data_joined.csv")
+
+
+surveys <- read.csv("/Users/camilavargaspoulsen/github/practicepracticepractice/Carpentry/R_ecology_lesson/portal_data_joined.csv")
+
---
+++ Carpentry/R_ecology_lesson/teaching_demo.R
@@ -0,0 +1,8 @@
+###R for ecology Carpentry lesson
+
+download.file(url = "http://ndownloader.figshare.com/files/2292169",
+              destfile = "data_raw/portal_data_joined.csv")
+
+
+
+
